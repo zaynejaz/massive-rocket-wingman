e9e2c8efab8a7015a2a9c9af8d1e80c14f598cbb
zaynejaz <zayn.ejaz@gmail.com>
2023-02-16 15:17:47 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Welcome to Wingman'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means model will take more risks.
+      max_tokens: 3000, // Maximum number of tokens to generate in completion. Most models have a context length of 2048 tokens (except for newest models, which support 4096).
+      top_p: 1, // Alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Server unavailable');
+  }
+})
+
+// app.listen(5001, () => console.log('Wingman server started on http://127.0.0.1:5001/'))
+
+app.listen(5001, () => console.log('Wingman server started on  http://localhost:5001'))
